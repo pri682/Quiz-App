b16271cd83b8ffe8b2a5279053c5e2b4c8a89e32
pri682 <priyankakarki474@gmail.com>
2024-08-20 12:45:40 -0500

    New highscores files

--- end.js
+++ end.js
@@ -4,6 +4,12 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 finalScore.innerText = mostRecentScore;
 
+const highScores=JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGHSCORES = 9;
+
+finalScore.innerText = mostRecentScore; 
+
 username.addEventListener('keyup', () =>{
     console.log(username.value);
     savescorebtn.disabled = !username.value;
@@ -12,4 +18,17 @@ username.addEventListener('keyup', () =>{
 saveHighScore = e => {
     console.log("clicked the save button!");
     e.preventDefault();
+
+const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+};
+highScores.push(score);
+highScores.sort( (a, b) => b.score - a.score)
+highScores.splice(9);
+
+localStorage.setItem("highScores", JSON.stringify(highScores));
+window.location.assign("/");
+
+
 };
